cfde39fe16dbaea1a6312f777cb7a9b13e7f6ca2
Jan van Brügge <supermanitu@gmail.com>
2017-04-08 11:19:54 +0200

    Add promise helper

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,6 @@
 export { diagramArbitrary } from './diagramArbitrary';
+
+export function promise(run : (err: any) => void) : Promise<boolean>
+{
+    return new Promise((resolve, reject) => run(err => err ? reject(err) : resolve(true)));
+}
