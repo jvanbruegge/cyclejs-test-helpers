3d70426e38cd819d48bb29aa69363a1ac2e79289
Jan van Brügge <supermanitu@gmail.com>
2017-04-26 19:05:06 +0200

    Add withTime helper

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,18 @@
+import { mockTimeSource, MockTimeSource } from '@cycle/time';
+
 export { diagramArbitrary } from './diagramArbitrary';
 
 export function promise(run : (err: any) => void) : Promise<boolean>
 {
     return new Promise((resolve, reject) => run(err => err ? reject(err) : resolve(true)));
+}
+
+export function withTime(test : (Time : MockTimeSource) => void) {
+    return function() {
+        const Time = mockTimeSource();
+
+        test(Time);
+
+        return promise(Time.run);
+    }
 }
