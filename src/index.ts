4fadb69f214a0f5e70e2ba30695f44ddbeb97bb6
Jan van Brügge <supermanitu@gmail.com>
2018-04-03 21:37:55 +0200

    Remove unnecessary function wrapper

--- src/index.ts
+++ src/index.ts
@@ -9,14 +9,12 @@ export function promise(run : (err: any) => void) : Promise<boolean>
     return new Promise((resolve, reject) => run(err => err ? reject(err) : resolve(true)));
 }
 
-export function withTime(test : (Time : MockTimeSource) => void) {
-    return function() {
-        const Time = mockTimeSource();
+export function withTime(test : (Time : MockTimeSource) => void): Promise<boolean> {
+    const Time = mockTimeSource();
 
-        test(Time);
+    test(Time);
 
-        return promise(Time.run);
-    }
+    return promise(Time.run);
 }
 
 export function addPrevState(main : any, prevState : any) : any {
